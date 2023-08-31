d3dee362c13e3a1f964136cccd9b63bf5ae48272
dominic <dominicf2001@gmail.com>
2023-08-31 16:46:15 -0400

    create skeleton structure for spawning, movement, and destruction

--- index.js
+++ index.js
@@ -1,13 +1,76 @@
+const path = [
+    { x: 50, y: 50 },
+    { x: 150, y: 50 },
+    { x: 150, y: 150 },
+    { x: 250, y: 150 },
+    { x: 250, y: 250 },
+];
+
+    
+class Enemy {
+    constructor(speed, id) {
+        this.speed = speed;
+        this.x = path[0].x,
+            this.y = path[0].y,
+            this.pathIndex = 0,
+            this.id = id
+    }
+
+    draw() {
+        // draw enemy
+        // note: should eventually depend on the enemy type
+        fill(50);
+        noStroke();
+        ellipse(this.x, this.y, 20, 20);
+
+        // calculate distance to next point
+        let dx = path[this.pathIndex + 1].x - this.x;
+        let dy = path[this.pathIndex + 1].y - this.y;
+        let distance = sqrt(dx * dx + dy * dy);
+
+        // if it cannot reach the next point in this frame
+        if (distance > this.speed) {
+            // normalize
+            this.x += (dx / distance) * this.speed;
+            this.y += (dy / distance) * this.speed;
+        } else {
+            this.x = path[this.pathIndex + 1].x;
+            this.y = path[this.pathIndex + 1].y;
+            this.pathIndex++;
+
+            // delete this enemy
+            if (this.pathIndex === path.length - 1) {
+                enemies.splice(this.id, 1);
+            }
+        }
+    }
+};
+
+const enemies = [
+    new Enemy(2, 0),
+    new Enemy(3, 1)
+];
+
 function setup() {
     createCanvas(400, 400);
 }
 
 function draw() {
-    background(200);
-    if (mouseIsPressed) {
-        fill(0);
-    } else {
-        fill(255);
+    background(0);
+
+    // draw path
+    strokeWeight(20);
+    stroke(255);
+    noFill();
+    beginShape();
+    for (let point of path) {
+        vertex(point.x, point.y);
     }
-    ellipse(mouseX, mouseY, 80, 80);
+    endShape();
+    
+    // draw enemies
+    for (const enemy of enemies) {
+        enemy.draw();
+    }
+    
 }
