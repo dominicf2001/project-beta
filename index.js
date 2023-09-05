89f0ffb90cab06207414a484d50f4c979b9c5c2f
dominic <dominicf2001@gmail.com>
2023-09-05 16:36:47 -0400

    combine enemy and tower logics

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import Enemy from "./enemy.js";
+import { Tower, Bullet }  from "./tower.js";
+
+// GLOBAL VARIABLES
 
 const path = [
     { x: 50, y: 50 },
@@ -13,13 +16,110 @@ const enemies = [
     new Enemy(3, path)
 ];
 
-function setup() {
-    createCanvas(400, 400);
+// tower variables
+const towerLimit = 5;
+const towers = [];
+const bullets = [];
+let dragTower = null;
+
+// EVENT LISTENERS
+
+window.mousePressed = function(event) {
+    console.log(event);
+
+    // Check if mouse is inside a tower
+    for(let t = 0; t < towers.length; t++) {
+        if (towers[t].mouseInside()) {
+            dragTower = towers.splice(t, 1)[0];
+            dragTower.hover = true;
+            towers.push(dragTower);
+            break;
+        }
+    }
+
+    //Ignore touch events, only handle left mouse button
+    if (event.button === 0 && !dragTower) {        
+        try {
+            if (towers.length > towerLimit) {
+                throw new Error("No more towers allowed!");
+            }
+            let t = new Tower(mouseX, mouseY);
+            towers.push(t);
+            bullets.push(new Bullet(t));
+        } catch (e) {
+            alert(e);
+        }
+    }
 }
 
-function draw() {
-    background(0);
+window.mouseDragged = function() {
+    // Move tower if it's being dragged
+    if(dragTower) {
+        dragTower.x = mouseX;
+        dragTower.y = mouseY;
+    }
+}
 
+window.mouseReleased = function() {
+    // Stop dragging tower
+    if (dragTower) {
+        dragTower.x = mouseX;
+        dragTower.y = mouseY;
+        dragTower.hover = false;
+        dragTower = null;
+    }
+}
+
+window.mouseMoved = function() {
+    // Change cursor if mouse is inside a tower
+    for(let t of towers) {
+        if (t.mouseInside()) {
+            t.hover = true;
+            cursor('grab');
+            return;
+        }
+    }
+    for(let t of towers) {
+        t.hover = false;
+    }
+    cursor();
+}
+
+// HELPERS
+
+function fireBullets() {
+    // Generate bullets for each tower
+    for(let t of towers) {
+        bullets.push(new Bullet(t));
+    }
+}
+
+// GAME LOOP
+
+window.setup = function() {
+    createCanvas(400, 400);
+
+    // Fire bullets every 400mps
+    setInterval(fireBullets, 400);
+}
+
+window.draw = function() {
+    background(200);
+
+    // Draw towers
+    for (const t of towers) {
+        t.draw();
+    }
+    
+    // Draw bullets first, so they appear behind towers
+    for (const i in bullets) {
+        if (bullets[i].isOutOfRange()) {
+            bullets.splice(bullets.indexOf(this), 1);
+        } else {
+            bullets[i].draw();   
+        }
+    }
+    
     // draw path
     strokeWeight(20);
     stroke(255);
@@ -34,14 +134,10 @@ function draw() {
     for (const i in enemies) {
         if (enemies[i].hasReachedEnd()) {
             enemies.splice(i, 1);
-        } else {1
-            enemies[i].draw();   
+        } else {
+            enemies[i].draw();
         }
     }
     
 }
 
-// In order to use ES6 modules, these functions need to be set on the window object.
-// This is due to p5 not being able to directly see into modules
-window.setup = setup;
-window.draw = draw;
--- tower.js
+++ tower.js
@@ -1,27 +1,20 @@
-const TOWER_SIZE = 20;
-
-let towerLimit = 5;
-let towers = [];
-let bullets = [];
-
-class Tower {
+export class Tower {
+    static TOWER_SIZE = 20;
+    
     constructor(x, y) {
-        if (towerLimit <= 0) {
-            throw new Error("No more towers allowed!");
-        }
         this.x = x;
         this.y = y;
         this.range = 50;
         this.damage = 1;
         this.fireRate = 1;
         this.hover = false;
-        towerLimit--;
     }
 
     draw() {
         fill(152, 84, 235);
-        rect(this.x - (TOWER_SIZE / 2), this.y - (TOWER_SIZE / 2), TOWER_SIZE, TOWER_SIZE);
+        rect(this.x - (Tower.TOWER_SIZE / 2), this.y - (Tower.TOWER_SIZE / 2), Tower.TOWER_SIZE, Tower.TOWER_SIZE);
 
+        strokeWeight(2);
         stroke(255);
         noFill();
         if(this.hover) {
@@ -31,11 +24,11 @@ class Tower {
     }
 
     mouseInside() {
-        return mouseX > this.x - (TOWER_SIZE / 2) && mouseX < this.x + (TOWER_SIZE / 2) && mouseY > this.y - (TOWER_SIZE / 2) && mouseY < this.y + (TOWER_SIZE / 2);
+        return mouseX > this.x - (Tower.TOWER_SIZE / 2) && mouseX < this.x + (Tower.TOWER_SIZE / 2) && mouseY > this.y - (Tower.TOWER_SIZE / 2) && mouseY < this.y + (Tower.TOWER_SIZE / 2);
     }
 };
 
-class Bullet {
+export class Bullet {
     constructor(tower) {
         this.x = tower.x;
         this.y = tower.y;
@@ -48,103 +41,14 @@ class Bullet {
     }
 
     draw() {
+        strokeWeight(2);
         fill(255, 0, 0);
         ellipse(this.x, this.y, 5, 5);
         this.x += this.xMove;
         this.y += this.yMove;
         this.range--;
-
-        // Remove bullet if it's out of range
-        if (this.range <= 0) {
-            bullets.splice(bullets.indexOf(this), 1);
-        }
     }
-}
-
-function setup() {
-    createCanvas(400, 400);
-
-    // Fire bullets every 400ms
-    setInterval(fireBullets, 400);
-}
-
-function fireBullets() {
-    // Generate bullets for each tower
-    for(let t of towers) {
-        bullets.push(new Bullet(t));
+    isOutOfRange() {
+        return this.range <= 0;
     }
-}
-
-function draw() {
-    background(0);
-
-    // Draw bullets first, so they appear behind towers
-    for (let b of bullets) {
-        b.draw();
-    }
-
-    // Draw towers
-    for(let t of towers) {
-        t.draw();
-    }
-}
-
-let dragTower = null;
-
-function mousePressed(event) {
-    console.log(event);
-
-    // Check if mouse is inside a tower
-    for(let t = 0; t < towers.length; t++) {
-        if (towers[t].mouseInside()) {
-            dragTower = towers.splice(t, 1)[0];
-            dragTower.hover = true;
-            towers.push(dragTower);
-            break;
-        }
-    }
-
-    //Ignore touch events, only handle left mouse button
-    if (event.button === 0 && !dragTower) {
-        try {
-            let t = new Tower(mouseX, mouseY);
-            towers.push(t);
-            bullets.push(new Bullet(t));
-        } catch (e) {
-            alert(e);
-        }
-    }
-}
-
-function mouseDragged() {
-    // Move tower if it's being dragged
-    if(dragTower) {
-        dragTower.x = mouseX;
-        dragTower.y = mouseY;
-    }
-}
-
-function mouseReleased() {
-    // Stop dragging tower
-    if (dragTower) {
-        dragTower.x = mouseX;
-        dragTower.y = mouseY;
-        dragTower.hover = false;
-        dragTower = null;
-    }
-}
-
-function mouseMoved() {
-    // Change cursor if mouse is inside a tower
-    for(let t of towers) {
-        if (t.mouseInside()) {
-            t.hover = true;
-            cursor('grab');
-            return;
-        }
-    }
-    for(let t of towers) {
-        t.hover = false;
-    }
-    cursor();
 }
