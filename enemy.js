ff92b83e593b72a4986afc3d63802222b1d7724b
dominic <dominicf2001@gmail.com>
2023-09-05 15:48:29 -0400

    Add jsdoc documentation

---
+++ enemy.js
@@ -0,0 +1,47 @@
+/** @module enemy */
+
+/** Class representing an enemy */
+export default class Enemy {
+    /**
+     * Constructs an enemy based on speed, and the path it will follow
+     * @param {number} speed - how quick an enemy moves along a path
+     * @param {array} path - a path the enemy will be drawn on
+     */
+    constructor(speed, path) {
+        this.speed = speed;
+        this.path = path;
+        this.pathIndex = 0;
+        this.x = this.path[0].x;
+        this.y = this.path[0].y;
+    }
+    draw() {
+        // draw enemy
+        // note: should eventually depend on the enemy type
+        fill(50);
+        noStroke();
+        ellipse(this.x, this.y, 20, 20);
+
+        // calculate distance to next point
+        let dx = this.path[this.pathIndex + 1].x - this.x;
+        let dy = this.path[this.pathIndex + 1].y - this.y;
+        let distance = sqrt(dx * dx + dy * dy);
+
+        // if it cannot reach the next point in this frame
+        if (distance > this.speed) {
+            // normalize
+            this.x += (dx / distance) * this.speed;
+            this.y += (dy / distance) * this.speed;
+        } else {
+            this.x = this.path[this.pathIndex + 1].x;
+            this.y = this.path[this.pathIndex + 1].y;
+            this.pathIndex++;
+        }
+    }
+    /**
+     * Returns true if the enemy has reached the end of its path, else returns false
+     * @returns {boolean}
+     */
+    hasReachedEnd() {
+        return this.pathIndex === this.path.length - 1;
+    }
+};
