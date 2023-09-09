7de1e4fed21730761fe0ea38edfb68c90db5dd3f
melissa-underwood <100211401+melissa-underwood@users.noreply.github.com>
2023-09-09 14:14:29 -0400

    Added start menu
    Press enter to start

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ import { Tower, Bullet }  from "./tower.js";
 
 // GLOBAL VARIABLES
 
+// 0 - main menu
+// 1 - start game
+var gameMode = 0;
+let f_Andale;
+
 const path = [
     { x: 50, y: 50 },
     { x: 150, y: 50 },
@@ -22,32 +27,39 @@ const towers = [];
 const bullets = [];
 let dragTower = null;
 
+// preload
+window.preload = function() {
+    f_Andale = loadFont('assets/Andale-Mono.ttf');
+}
+
 // EVENT LISTENERS
 
 window.mousePressed = function(event) {
-    console.log(event);
+    if (gameMode == 1) {
+        console.log(event);
 
-    // Check if mouse is inside a tower
-    for(let t = 0; t < towers.length; t++) {
-        if (towers[t].mouseInside()) {
-            dragTower = towers.splice(t, 1)[0];
-            dragTower.hover = true;
-            towers.push(dragTower);
-            break;
-        }
-    }
-
-    //Ignore touch events, only handle left mouse button
-    if (event.button === 0 && !dragTower) {        
-        try {
-            if (towers.length > towerLimit) {
-                throw new Error("No more towers allowed!");
+        // Check if mouse is inside a tower
+        for(let t = 0; t < towers.length; t++) {
+            if (towers[t].mouseInside()) {
+                dragTower = towers.splice(t, 1)[0];
+                dragTower.hover = true;
+                towers.push(dragTower);
+                break;
             }
-            let t = new Tower(mouseX, mouseY);
-            towers.push(t);
-            bullets.push(new Bullet(t));
-        } catch (e) {
-            alert(e);
+        }
+
+        //Ignore touch events, only handle left mouse button
+        if (event.button === 0 && !dragTower) {        
+            try {
+                if (towers.length > towerLimit) {
+                    throw new Error("No more towers allowed!");
+                }
+                let t = new Tower(mouseX, mouseY);
+                towers.push(t);
+                bullets.push(new Bullet(t));
+            } catch (e) {
+                alert(e);
+            }
         }
     }
 }
@@ -85,6 +97,12 @@ window.mouseMoved = function() {
     cursor();
 }
 
+window.keyPressed = function() {
+    if (keyCode === ENTER) {
+        gameMode = 1;
+    }
+}
+
 // HELPERS
 
 function fireBullets() {
@@ -104,42 +122,53 @@ window.setup = function() {
 }
 
 window.draw = function() {
-    background(200);
+    if (gameMode == 0) {
+        mainMenu();
+    }
+    if (gameMode == 1) {
+        background(200);
 
-    // Draw bullets first, so they appear behind towers
-    for (const i in bullets) {
-        if (bullets[i].isOutOfRange()) {
-            bullets.splice(i, 1);
-        } else {
-            bullets[i].draw();   
+        // Draw bullets first, so they appear behind towers
+        for (const i in bullets) {
+            if (bullets[i].isOutOfRange()) {
+                bullets.splice(i, 1);
+            } else {
+                bullets[i].draw();   
+            }
         }
-    }
 
-    // Draw towers
-    for (const t of towers) {
-        t.draw();
-    }
-    
-    // draw path
-    push();
-    strokeWeight(20);
-    stroke(255);
-    noFill();
-    beginShape();
-    for (const point of path) {
-        vertex(point.x, point.y);
-    }
-    endShape();
-    pop();
-    
-    // draw or remove enemies
-    for (const i in enemies) {
-        if (enemies[i].hasReachedEnd()) {
-            enemies.splice(i, 1);
-        } else {
-            enemies[i].draw();
+        // Draw towers
+        for (const t of towers) {
+            t.draw();
+        }
+        // draw path
+        push();
+        strokeWeight(20);
+        stroke(255);
+        noFill();
+        beginShape();
+        for (const point of path) {
+            vertex(point.x, point.y);
+        }
+        endShape();
+        pop();
+        
+        // draw or remove enemies
+        for (const i in enemies) {
+            if (enemies[i].hasReachedEnd()) {
+                enemies.splice(i, 1);
+            } else {
+                enemies[i].draw();
+            }
         }
     }
     
 }
 
+function mainMenu() {
+    background('#262626');
+    textFont(f_Andale);
+    textAlign(CENTER);
+    text("Press [ENTER] to start", 200, 300);
+    fill('#FFF');
+}
