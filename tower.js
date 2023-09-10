4bc3883c08615889521deb7a124be460f6861449
Yaseen Shaikh <yshaikh@kent.edu>
2023-09-10 14:25:51 -0400

    Added a placeholder tower spritesheet
    Also tried setting up, no success yet but haven't worked on it for too long

--- tower.js
+++ tower.js
@@ -1,6 +1,15 @@
+var spriteSheet;
+var towerAnimation;
+
+function preload(){
+    spriteSheet = loadSpriteSheet('assets/RedMoonTower_free_idle_animation.png', 100, 140, 11);
+    towerAnimation = loadAnimation(spriteSheet)
+}
+
 export class Tower {
     static TOWER_SIZE = 20;
     
+
     constructor(x, y) {
         this.x = x;
         this.y = y;
@@ -9,7 +18,7 @@ export class Tower {
         this.fireRate = 1;
         this.hover = false;
     }
-
+    
     draw() {
         push();
         fill(152, 84, 235);
