c5b1ba623651a2b07dd052c67d1552178ed74406
Kenneth Guzy <kenguzy@gmail.com>
2023-11-11 17:28:43 -0500

    added tower image into index.css

--- ui-handler.js
+++ ui-handler.js
@@ -35,7 +35,7 @@ export class UIHandler {
         this.enemyStandard;
         this.enemySummoner;
         this.enemySummonee;
-
+        this.standardTowerImg;
     }
 
     preloadAssets() {
@@ -44,6 +44,7 @@ export class UIHandler {
         this.enemyStandard = loadImage('./assets/Basic_Enemy.png');
         this.enemySummoner = loadImage('./assets/Summoner.png');
         this.enemySummonee = loadImage('./assets/Summonee.png');
+        this.standardTowerImg = loadImage('./assets/RedMoonTower.png');
     }
 
     initializeUI() {
